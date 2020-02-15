735f0f4a6b27850be81f9b25f9dd0bef0f951cda
Rohith Maddali <1015083@student.sae.edu.au>
2020-02-15 19:36:13 +1100

    Added level 6 and pseudo code for AI

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gets player stats
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //displays the current health of AI.
+        //gets player's choice of difficulty from previous round.
+        //gives players previous round difficulty chosen for ai as player's current round difficulty.
+        //updates AI health based on the difficulty given by the player.
+    }
+}
