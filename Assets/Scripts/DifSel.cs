ed65caa6b022306dd54bb9567dded406d5fd21c1
Rohith Maddali <1015083@student.sae.edu.au>
2020-02-20 21:28:38 +1100

    Started AI script

---
+++ Assets/Scripts/DifSel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DifSel : MonoBehaviour
+{
+    //create list for hard levels.
+    public List<GameObject> Hard = new List<GameObject>(GameObject.FindGameObjectsWithTag("Hard"));
+    //create list for easy levels.
+    public List<GameObject> Easy = new List<GameObject>(GameObject.FindGameObjectsWithTag("Easy"));
+
+    // Start is called before the first frame update
+    public enum Difficulty
+    {
+        Hard,
+        Easy
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
